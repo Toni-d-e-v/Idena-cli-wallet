ce5ce2448a4dd514399e8d7d72447ba4d04be79b
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2021-05-06 19:00:03 +0200

    Code clean up+ last tx

--- main.py
+++ main.py
@@ -1,82 +1,60 @@
 from api import IdenaAPI
-
-# Connect to local node with default settings (with no API key)
+# Connect to local node with default settings (with no Custom api)
 api = IdenaAPI()
-
-# Connect to local node with default settings (with API key)
 api = IdenaAPI(api_key=input("API KEY:"))
 myaddr = api.address()
 mybalance = api.balance(myaddr["result"])
 sync = api.sync_status()
 nodever = api.node_version()
-
-
-
 # Check balance
-
 print("Welcome to Idena-CLI wallet v0.1")
 print("Made by Toni.Dev, Donation:0x6357cdf056b0dca75a14094987fa7eabbd78be5f")
 print("Coinbase Address:", myaddr["result"])
 print("Balance:", mybalance["result"]["balance"])
 print("Is it syncing:", sync["result"]["syncing"])
 print("Node Version:", nodever["result"])
-
 print("1.SEND IDNA")
 print("2.START MINING")
 print("3.STOP MINING")
-print("4.EXIT")
+print("4.LIST LAST TX")
+print("5.EXIT")
 def home_act1():
             sendto = input("Send to:")
             amount = input("Amount:")
             sure = input("ARE YOU SURE: Y/N:")
             if sure == 'Y':
-               
-
                 result = api.send(myaddr["result"], sendto, amount)
                 print("TX ID:", result["result"], "Sended to:", sendto, "Amount:", amount)
             else:
                 exit()
-
 def home_act2():
     start = api.go_online()
     print("Started Mining")
-
-
 def home_act3():
     stop = api.go_offline()
     print("Stoped Mining")
-
-
-def home_act4():
+def home_act5():
     print("You closed Idena Cli Wallet")
     exit()
-
-
-
+def home_act4():
+    tx = api.transactions(myaddr["result"], 1)
+    print("Last Transaction:", "Hash:", tx["result"]["transactions"][0]["hash"], "To:", tx["result"]["transactions"][0]["to"], "Amount:", tx["result"]["transactions"][0]["amount"])
 def action():
     loop = True
     while loop:
         # print(menu)
-        player_action = input("CHOSE FROM MENU ABOVE:")
-
-        if player_action == '1':
+        user_action = input("CHOSE FROM MENU ABOVE:")
+        if user_action == '1':
             return home_act1() 
-
-
-        elif player_action == '2':
+        elif user_action == '2':
             return home_act2()
-
-        elif player_action == '3':
+        elif user_action == '3':
             return home_act3()
-        elif player_action == '4':
+        elif user_action == '4':
             return home_act4()
-
+        elif user_action == '5':
+            return home_act5()
         else:
-            print("Please type \'1\', \'2\', \'3\', or \'4\'")
-
+            print("Please type \'1\', \'2\', \'3\', or \'4\, \5\'")
 action()
-
-
-
-
 #There is no virus in here be free to keep looking :)
